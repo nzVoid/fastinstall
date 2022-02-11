69d725cfd67888f79d98531dca42356acd25bf82
nzVoid <yakunin901@gmail.com>
2022-02-11 21:47:41 +0300

    11.02

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#ngrok install
+sudo apt install unzip wget
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+sudo mv ./ngrok /usr/bin/ngrok
