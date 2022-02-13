1b82127b22dd5f5b61790de14fd9e42a92fc4087
nzVoid <yakunin901@gmail.com>
2022-02-13 10:09:21 +0300

    13.02

--- install.sh
+++ install.sh
@@ -5,3 +5,23 @@ sudo apt install unzip wget
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 sudo mv ./ngrok /usr/bin/ngrok
+
+#vim
+sudo apt install vim
+
+#preload
+sudo apt install preload
+
+#git
+sudo apt install git
+
+#htop
+sudo apt install htop
+
+#python3
+sudo apt install python3
+sudo apt install python3-pip
+
+#speedtest-cli
+sudo apt install speetest-cli
+
